259418704d95bafb9e29547af0b758c832372dbe
krzys-h <krzys_h@interia.pl>
2016-10-15 19:18:56 +0200

    Configure djangorestframework

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,11 @@
-from django.conf.urls import url
-
+from django.conf.urls import url, include
+from rest_framework.routers import DefaultRouter
 from . import views
 
+router = DefaultRouter()
+router.register(r'users', views.UserViewSet)
+
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls')),
 ]
--- backend/views.py
+++ backend/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from .serializers import UserSerializer
 
-# Create your views here.
 
-
-def index(request):
-    return HttpResponse("Hello world!")
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
