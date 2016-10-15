b96823b3355553c535d951e3f32bac7c23c03891
krzys-h <krzys_h@interia.pl>
2016-10-15 22:18:58 +0200

    Add first and last name to user info

--- backend/serializers.py
+++ backend/serializers.py
@@ -5,4 +5,4 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('url', 'username')
+        fields = ('url', 'username', 'first_name', 'last_name')
