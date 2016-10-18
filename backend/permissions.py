3f6d455d23ed50ea4876cf3944cd9ae955d0c5b9
krzys-h <krzys_h@interia.pl>
2016-10-18 21:39:05 +0200

    Don't allow creation of new users via POST

--- backend/permissions.py
+++ backend/permissions.py
@@ -1,3 +1,5 @@
+import logging
+
 from rest_framework import permissions
 
 
@@ -6,7 +8,15 @@ class IsSelfOrReadOnly(permissions.BasePermission):
     Custom permission to only allow users themselves to edit their profiles
     """
 
+    def has_permission(self, request, view):
+        if view.action == 'create':
+            return False
+
+        return True
+
     def has_object_permission(self, request, view, obj):
+        logging.info(request.method)
+
         # Read permissions are allowed to any request
         if request.method in permissions.SAFE_METHODS:
             return True
@@ -16,4 +26,4 @@ class IsSelfOrReadOnly(permissions.BasePermission):
             return False
 
         # Write permissions are only allowed to the user himself
-        return obj.pk == request.user.pk
+        return obj == request.user
