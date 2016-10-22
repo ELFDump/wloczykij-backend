25e8aff05d7246732070f171ee9871efc606f9bb
krzys-h <krzys_h@interia.pl>
2016-10-22 21:50:41 +0200

    Fix handling of EXIF rotation data

--- backend/models.py
+++ backend/models.py
@@ -2,11 +2,15 @@ import os
 import time
 
 import stdimage
+from PIL import Image
 from django.contrib.auth.models import User
 from django.contrib.gis.db import models as gis_models
+from django.core.files.base import ContentFile
 from django.db import models
 from django.db.models.signals import post_save
 from django.utils.encoding import force_bytes
+from six import BytesIO
+from stdimage.utils import render_variations
 
 
 class UserProfile(models.Model):
@@ -50,8 +54,41 @@ def photo_path(instance, filename):
     return 'photos/{0}/{1}{2}'.format(instance.place.id, int(round(time.time() * 1000)), os.path.splitext(filename)[1])
 
 
+# see https://github.com/codingjoe/django-stdimage/issues/66#issuecomment-180309788
+def photo_render_variations(file_name, variations, storage):
+    with storage.open(file_name) as f:
+        with Image.open(f) as image:
+            file_format = image.format
+            exif = image._getexif()
+
+            # if image has exif data about orientation, let's rotate it
+            orientation_key = 274  # cf ExifTags
+            if exif and orientation_key in exif:
+                orientation = exif[orientation_key]
+
+                rotate_values = {
+                    3: Image.ROTATE_180,
+                    6: Image.ROTATE_270,
+                    8: Image.ROTATE_90
+                }
+
+                if orientation in rotate_values:
+                    image = image.transpose(rotate_values[orientation])
+
+                file_buffer = BytesIO()
+                image.save(file_buffer, file_format)
+                f = ContentFile(file_buffer.getvalue())
+                storage.delete(file_name)
+                storage.save(file_name, f)
+
+    # render stdimage variations
+    render_variations(file_name, variations, replace=True, storage=storage)
+
+    return False  # prevent default rendering
+
+
 class Photo(models.Model):
-    photo = stdimage.StdImageField(upload_to=photo_path, variations={'resized': {'width': 2048, 'height': 2048}})
+    photo = stdimage.StdImageField(upload_to=photo_path, render_variations=photo_render_variations, variations={'resized': {'width': 2048, 'height': 2048}})
     place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='photos')
 
     def __str__(self):
